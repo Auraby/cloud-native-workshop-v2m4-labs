52db63f6550d7ec63a5a220d7bd46c37bf33a507
Tan Alwin <tanalwin97@gmail.com>
2022-10-27 14:50:14 +0800

    Add logging settings for gunicorn

--- admin-service/admin/main.py
+++ admin-service/admin/main.py
@@ -1,5 +1,6 @@
 from flask_cors import CORS
 from admin import app, app_blueprint, api, PORT
+import logging
 
 from admin.health_status_api import health_ns
 from admin.catalog_manager_api import catalog_manager_ns
@@ -9,6 +10,11 @@ api.add_namespace(catalog_manager_ns)
 
 CORS(app)
 app.register_blueprint(app_blueprint)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == '__main__':
     app.run(port=PORT, debug=True)
